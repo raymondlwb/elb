5179c233d155ac6eeea556ac7661d3562485f265
tonic <tonicbupt@gmail.com>
2016-04-29 16:55:38 +0800

    add path for 斌哥

--- app.lua
+++ app.lua
@@ -1,11 +1,35 @@
 local utils = require "utils"
 
-backend, _ = cache:get(ngx.var.host)
+-- 默认还是用host
+-- e.g. www.ricebook.net
+local key = ngx.var.host
+
+-- 如果有path就尝试第一级path
+-- 第一级path拼host上
+-- e.g. www.ricebook.net/first_path/
+if ngx.var.uri ~= '/' then
+    local first_path = utils.split(ngx.var.uri, '/', 2)[2]
+    key = key..first_path..'/'
+end
+
+backend, _ = cache:get(key)
 if not backend then
-    backend = utils.get_from_servernames(ngx.var.host)
+    -- 尝试用带path的去取
+    -- cache key也带path
+    -- 或者这里没有就还是原来的host
+    local cache_key = key
+    backend = utils.get_from_servernames(key)
+
+    -- 还是没有就用默认的host去取
+    -- cache key变成默认的host
+    if not backend then
+        backend = utils.get_from_servernames(ngx.var.host)
+        cache_key = ngx.var.host
+    end
+    -- 即使没有命中也加上cache好了
+    -- 不然容易拖死? 不至于吧...
     -- 60s ttl
-    cache:set(ngx.var.host, backend, 60)
+    cache:set(cache_key, backend, 60)
 end
 
 ngx.var.backend = backend
-
--- utils.lua
+++ utils.lua
@@ -3,6 +3,31 @@ local _M = {}
 local analysis = ngx.shared.analysis
 local servernames = ngx.shared.servernames
 
+function _M.split(str, sSeparator, nMax, bRegexp)
+    assert(sSeparator ~= '')
+    assert(nMax == nil or nMax >= 1)
+
+    local aRecord = {}
+
+    if str:len() > 0 then
+        local bPlain = not bRegexp
+        nMax = nMax or -1
+
+        local nField=1 nStart=1
+        local nFirst,nLast = str:find(sSeparator, nStart, bPlain)
+        while nFirst and nMax ~= 0 do
+            aRecord[nField] = str:sub(nStart, nFirst-1)
+            nField = nField+1
+            nStart = nLast+1
+            nFirst,nLast = str:find(sSeparator, nStart, bPlain)
+            nMax = nMax-1
+        end
+        aRecord[nField] = str:sub(nStart)
+    end
+
+    return aRecord
+end
+
 function _M.read_data()
     ngx.req.read_body()
     local data = cjson.decode(ngx.req.get_body_data())
