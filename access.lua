cfa050e14c410c3a63569d9896e4fc511e64e57b
Zhang Ye <zhangye@ricebook.com>
2016-08-09 19:18:32 +0800

    app invite

--- access.lua
+++ access.lua
@@ -43,7 +43,7 @@ end
 
 if ngx.var.host == 'enjoy.ricebook.com' or ngx.var.host ==  'enjoytest.ricebook.com' then
     domain_flag = false
-    local fp, err = ngx.re.match(first_path, '(exhibit|order|trace|user|api|manger)')
+    local fp, err = ngx.re.match(first_path, '(exhibit|order|trace|user|api|manger|invite)')
     if err ~= nil then
         ngx.log(ngx.ERR, err)
         ngx.exit(ngx.HTTP_BAD_GATEWAY)
