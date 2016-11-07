82e5addd91548888df01df55a499cb85185e361a
Zhang Ye <zhangye@ricebook.com>
2016-11-07 12:04:10 +0800

    qps data to statsd

--- init/init_var.lua
+++ init/init_var.lua
@@ -7,7 +7,7 @@ cjson = require 'cjson'
 cache = lrucache.new(200)
 reqlimit = req.new('filter_indicator', 'filter_storage')
 refcheck = ref.new('ref_storage')
-
+statsd = require 'lib.statsd'
 
 if not cache then
     ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
