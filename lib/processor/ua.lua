0cd8bc9ab0bb25d81b8967e32c41cfac1a1eb807
Zhang Ye <zhangye@ricebook.com>
2016-11-17 20:24:44 +0800

    ua match has some problem

--- lib/processor/ua.lua
+++ lib/processor/ua.lua
@@ -1,10 +1,18 @@
+string = require 'string'
 local _M = {}
 
 function _M.process(conditions)
     for i = 1, #conditions, 1 do
         key = conditions[i]['condition']
-        local captured, err = ngx.re.match(ngx.var.http_user_agent, key)
+        if not  ngx.var.http_user_agent then
+            ngx.log(ngx.ERR, "ua is nil")
+            return nil
+        end
+        local ua = string.lower(ngx.var.http_user_agent)
+        local captured, err = ngx.re.match(ua, key)
         if err ~= nil then
+            ngx.log(ngx.ERR, "ngx.re.match failed: "..err)
+            ngx.log(ngx.ERR, "input ua: "..ua)
             return nil
         end
         if captured ~= nil then
