0654af30f6435432cd6ad1d54d0c880bd4065e4b
Zhang Ye <zhangye@ricebook.com>
2016-11-29 16:11:10 +0800

    modify env name for statsd

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ _M.REDIS_HOST = os.getenv("REDIS_HOST") or '127.0.0.1'
 _M.REDIS_PORT = os.getenv("REDIS_PORT") or '6379'
 _M.NAME = os.getenv("ELBNAME") or 'ELB'
 
-_M.STATSD_HOST = os.getenv("STATSD") or '10.10.245.111'
+_M.STATSD_HOST = os.getenv("STATSD_HOST") or 'localhost'
 _M.STATSD_PORT = os.getenv("STATSD_PORT") or 8125
 _M.STATSD_FORMAT = 'elb3_domain_stat.%s.%s'
 
